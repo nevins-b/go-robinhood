2f1fa5ca6a69ae584802c4cb33dca7546ac3ead9
Nevins Bartolomeo <nbartolomeo@gmail.com>
2017-05-19 17:56:49 -0400

    updating tests, some minor cleanup

--- hashtable.go
+++ hashtable.go
@@ -2,23 +2,22 @@ package robinhood
 
 import (
 	"bytes"
-	"hash/adler32"
-	"log"
+	"hash/fnv"
 )
 
 const InitialSize = 256
-const LoadFactorPercent = 90
+const LoadFactorPercent = 98
 
 type elem struct {
 	key   []byte
-	value []byte
+	value interface{}
 	hash  uint32
 }
 
 type HashTable struct {
 	buffer          []elem
-	resizeThreshold int
-	numElements     int
+	resizeThreshold uint32
+	numElements     uint32
 	mask            uint32
 }
 
@@ -31,11 +30,15 @@ func NewHashTable() *HashTable {
 	}
 }
 
+func (h *HashTable) capacity() uint32 {
+	return uint32(cap(h.buffer))
+}
+
 func (h *HashTable) hashKey(key []byte) uint32 {
-	hash := adler32.New()
+	hash := fnv.New32a()
 	hash.Write(key)
 	sum := hash.Sum32()
-	sum &= 0x7fffffff
+	sum &= uint32(0x7fffffff)
 	if sum == 0 {
 		sum |= 1
 	}
@@ -43,30 +46,27 @@ func (h *HashTable) hashKey(key []byte) uint32 {
 }
 
 func (h *HashTable) grow() {
-	log.Printf("Growing buffer")
-	buf := make([]elem, 2*cap(h.buffer), 2*cap(h.buffer))
+	newCap := 2 * h.capacity()
+	buf := make([]elem, newCap, newCap)
 	copy(buf, h.buffer)
 	h.buffer = buf
-	h.resizeThreshold = (cap(h.buffer) * LoadFactorPercent) / 100
-	h.mask = uint32(cap(h.buffer) - 1)
+	h.resizeThreshold = (newCap * LoadFactorPercent) / 100
+	h.mask = h.capacity() - 1
 }
 
 func (h *HashTable) isDeleted(hash uint32) bool {
-	return (hash >> 31) != 0
+	return hash>>31 != 0
 }
 
 func (h *HashTable) desiredPos(hash uint32) uint32 {
-	pos := hash & h.mask
-	log.Printf("Got position: %d, %d, %d", hash, h.mask, pos)
-	return pos
+	return hash & h.mask
 }
 
 func (h *HashTable) probeDistance(hash, slotIndex uint32) uint32 {
-	return (slotIndex + uint32(cap(h.buffer)) - h.desiredPos(hash)) & h.mask
+	return (slotIndex + h.capacity() - h.desiredPos(hash)) & h.mask
 }
 
 func (h *HashTable) elemHash(ix uint32) uint32 {
-	log.Printf("Trying to get hash for pos: %d", ix)
 	return h.buffer[ix].hash
 }
 
@@ -79,17 +79,14 @@ func (h *HashTable) insertHelper(hash uint32, key, value []byte) {
 		value: value,
 	}
 	for {
-		if h.elemHash(pos) == 0 {
-			h.buffer[pos] = elem{
-				hash:  hash,
-				key:   key,
-				value: value,
-			}
+		q := h.elemHash(pos)
+		if q == 0 {
+			h.buffer[pos] = e
 			return
 		}
-		existingElemProdDist := h.probeDistance(h.elemHash(pos), pos)
+		existingElemProdDist := h.probeDistance(q, pos)
 		if existingElemProdDist < dist {
-			if h.isDeleted(h.elemHash(pos)) {
+			if h.isDeleted(q) {
 				h.buffer[pos] = e
 				return
 			}
@@ -108,11 +105,12 @@ func (h *HashTable) lookupIndex(key []byte) int {
 	pos := h.desiredPos(hash)
 	dist := uint32(0)
 	for {
-		if h.elemHash(pos) == 0 {
+		q := h.elemHash(pos)
+		if q == 0 {
 			return -1
-		} else if dist > h.probeDistance(h.elemHash(pos), pos) {
+		} else if dist > h.probeDistance(q, pos) {
 			return -1
-		} else if h.elemHash(pos) == hash && bytes.Equal(h.buffer[pos].key, key) {
+		} else if q == hash && bytes.Equal(h.buffer[pos].key, key) {
 			return int(pos)
 		}
 		pos = (pos + 1) & h.mask
@@ -128,10 +126,10 @@ func (h *HashTable) Insert(key, value []byte) {
 	h.numElements++
 }
 
-func (h *HashTable) Find(key []byte) *[]byte {
+func (h *HashTable) Find(key []byte) interface{} {
 	ix := h.lookupIndex(key)
 	if ix != -1 {
-		return &h.buffer[ix].value
+		return h.buffer[ix].value
 	}
 	return nil
 }
@@ -141,22 +139,22 @@ func (h *HashTable) Erase(key []byte) bool {
 	if ix == -1 {
 		return false
 	}
-	h.buffer[ix].hash |= 0x80000000
+	h.buffer[ix].hash = uint32(0x80000000)
 	h.numElements--
 	return true
 }
 
 func (h *HashTable) Size() int {
-	return h.numElements
+	return int(h.numElements)
 }
 
 func (h *HashTable) AverageProbeCount() float32 {
 	probeTotal := uint32(0)
-	for i := 0; i < h.numElements; i++ {
+	for i := 0; i < int(h.capacity()); i++ {
 		hash := h.elemHash(uint32(i))
 		if hash != 0 && !h.isDeleted(hash) {
 			probeTotal += h.probeDistance(hash, uint32(i))
 		}
 	}
-	return float32(probeTotal / uint32(h.numElements))
+	return float32(probeTotal / h.numElements)
 }
--- hashtable_test.go
+++ hashtable_test.go
@@ -1,9 +1,12 @@
 package robinhood
 
 import (
+	"bufio"
 	"bytes"
-	"fmt"
+	"log"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -12,7 +15,7 @@ func TestHashTableInsert(t *testing.T) {
 	h := NewHashTable()
 	h.Insert([]byte("test"), []byte("test"))
 	h.Insert([]byte("test"), []byte("test"))
-	assert.True(t, bytes.Equal(*h.Find([]byte("test")), []byte("test")))
+	assert.True(t, bytes.Equal(h.Find([]byte("test")).([]byte), []byte("test")))
 	assert.Equal(t, 2, h.Size(), "Check number of items")
 }
 
@@ -20,9 +23,14 @@ func TestHashTableCollision(t *testing.T) {
 	h := NewHashTable()
 	h.Insert([]byte("costarring"), []byte("costarring"))
 	h.Insert([]byte("liquid"), []byte("liquid"))
-	assert.True(t, bytes.Equal(*h.Find([]byte("costarring")), []byte("costarring")))
+	assert.True(t, bytes.Equal(h.Find([]byte("costarring")).([]byte), []byte("costarring")))
 	h.Erase([]byte("costarring"))
+	assert.True(t, bytes.Equal(h.Find([]byte("liquid")).([]byte), []byte("liquid")))
 	h.Insert([]byte("costarring"), []byte("costarring"))
+	h.Insert([]byte("costarring"), []byte("costarring1"))
+	h.Erase([]byte("costarring"))
+	h.Insert([]byte("costarring"), []byte("costarring2"))
+	h.Insert([]byte("costarring"), []byte("costarring3"))
 }
 
 func TestHashTableErase(t *testing.T) {
@@ -32,12 +40,42 @@ func TestHashTableErase(t *testing.T) {
 	assert.True(t, h.Erase([]byte("test")))
 	assert.Equal(t, 0, h.Size(), "Check number of items")
 	assert.Nil(t, h.Find([]byte("test")), "Check nil response")
+	h.Insert([]byte("test"), []byte("test"))
 }
 
 func TestHashTableGrow(t *testing.T) {
-	h := NewHashTable()
-	for i := 1; i <= 256; i++ {
-		h.Insert([]byte(fmt.Sprintf("test%d", i)), []byte("test"))
-		assert.Equal(t, i, h.Size(), "Check number of items")
+	file, err := os.Open("100k.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer file.Close()
+
+	h := NewHashTable()
+	scanner := bufio.NewScanner(file)
+	start := time.Now()
+	for scanner.Scan() {
+		h.Insert(scanner.Bytes(), scanner.Bytes())
+	}
+	log.Printf("Insert took: %s", time.Since(start))
+	file.Seek(0, 0)
+	scanner = bufio.NewScanner(file)
+	start = time.Now()
+	i := 0
+	max := 10000
+	for scanner.Scan() {
+		h.Erase(scanner.Bytes())
+		i++
+		if i >= max {
+			break
+		}
+	}
+	log.Printf("Erase took: %s", time.Since(start))
+	file.Seek(0, 0)
+	scanner = bufio.NewScanner(file)
+	start = time.Now()
+	for scanner.Scan() {
+		h.Find(scanner.Bytes())
+	}
+	log.Printf("Lookup took: %s", time.Since(start))
+	log.Printf("Average Probe Count: %f", h.AverageProbeCount())
 }
